d941a88a07c232bc9e66cfbcce1b51a26d4d5b76
Alex <nelsona3017@gmail.com>
2025-07-09 01:30:11 -0400

    Created modal for about me and contact, not yet fully implemented with pop-up functionality so it is hidden for now.

---
+++ index.js
@@ -0,0 +1,31 @@
+// template_kqa3d8i
+// service_399egvl
+// rH_cFtmEWf4FkK_Zs
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_399egvl",
+      "template_kqa3d8i",
+      event.target,
+      "rH_cFtmEWf4FkK_Zs"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable, please contact me direclty at nelsona3017@gmail.com."
+      );
+    });
+
+  setTimeout(() => {
+    console.log("bruh");
+  }, 1000);
+}
