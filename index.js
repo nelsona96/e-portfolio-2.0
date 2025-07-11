f937c97a34c3dc2ee499e1f33f4b267b89eb672f
Alex <nelsona3017@gmail.com>
2025-07-10 23:38:41 -0400

    Added dark theme styles in css and toggle in js, added moving shapes that move with the mouse position, temporarily removed all media queries to work alongside school video on responsiveness. Also made fixed contact button hide behind footer when scrolling all the way down.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// template_kqa3d8i
-// service_399egvl
-// rH_cFtmEWf4FkK_Zs
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
 function contact(event) {
   event.preventDefault();
@@ -24,18 +24,38 @@ function contact(event) {
         "The email service is temporarily unavailable, please contact me direclty at nelsona3017@gmail.com."
       );
     });
-
-  setTimeout(() => {
-    console.log("bruh");
-  }, 1000);
 }
 
-let isModalOpen = false;
+function moveShapes(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+}
+
+// A WAY TO TOGGLE CSS CLASSES ON HTML ELEMENTS
 function toggleModal() {
   if (isModalOpen) {
-    isModalOpen = false
+    isModalOpen = false;
     return document.body.classList.remove("modal--open");
   }
   isModalOpen = true;
-  document.body.classList += " modal--open"
+  document.body.classList += " modal--open";
+  // document.body.classList.toggle("modal--open");
+}
+
+// ANOTHER WAY TO TOGGLE CSS CLASSES ON HTML ELEMENTS
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+  // document.body.classList.toggle("dark-theme");
 }
